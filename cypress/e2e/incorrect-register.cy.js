b07e5c1586531c95dffc19dac31c0ce7d7de7ae2
Thiago Nunes <devthiart@gmail.com>
2025-11-18 17:26:58 -0300

    Testing sad path on register

---
+++ cypress/e2e/incorrect-register.cy.js
@@ -0,0 +1,10 @@
+describe('Register page', () => {
+  it('Filling out the form fields incorrectly and displaying error messages to the user', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('[data-test="register-button"]').click();
+    cy.get('[data-test="submit-button"]').click();
+    cy.contains('É necessário informar um endereço de email').should('be.visible');
+    cy.contains('Crie uma senha').should('be.visible');
+    cy.contains('Repita a senha criada acima').should('be.visible');
+  });
+});
