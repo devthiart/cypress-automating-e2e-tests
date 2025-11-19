1713266c7e6c4926c8f3c3d47cf3d55132762b16
Thiago Nunes <devthiart@gmail.com>
2025-11-19 10:59:33 -0300

    Correcting the exercises

--- cypress/e2e/exercises-class-02.cy.js
+++ cypress/e2e/exercises-class-02.cy.js
@@ -4,15 +4,15 @@ describe('Exercises - Class 02', () => {
   });
 
   it("The AdoPet title must be correctly", () => {
-    cy.title().should('include', 'AdoPet');
+    cy.title().should('eq', 'AdoPet');
   });
 
   it("Checking if the text 'Quem ama adota!' is present in the document", () => {
-    cy.contains("Quem ama adota!").should('be.visible');
+    cy.contains('p','Quem ama adota!').should('be.visible');
   });
 
   it("Checking if the text 'Adotar pode mudar...' is present in the document", () => {
-    cy.contains("Adotar pode mudar uma vida. Que tal buscar seu novo melhor amigo hoje? Vem com a gente!").should('be.visible');
+    cy.contains('p', 'Adotar pode mudar uma vida. Que tal buscar seu novo melhor amigo hoje? Vem com a gente!').should('be.visible');
   });
 
   it("testing login via a different flow", () => {
