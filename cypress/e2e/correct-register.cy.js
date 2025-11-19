aae3da3e5d94639dfdb08fd0dc8b7b5ae2a45360
Thiago Nunes <devthiart@gmail.com>
2025-11-19 15:06:13 -0300

    Creating register custom command

--- cypress/e2e/correct-register.cy.js
+++ cypress/e2e/correct-register.cy.js
@@ -5,10 +5,6 @@ describe('Register page', () => {
   });
   
   it('Must fill in the form fields correctly to register a new user', () => {
-    cy.get('[data-test="input-name"]').type('Thiago André');
-    cy.get('[data-test="input-email"]').type('thiagoandre@email.com');
-    cy.get('[data-test="input-password"]').type('Senha123');
-    cy.get('[data-test="input-confirm-password"]').type('Senha123');
-    cy.get('[data-test="submit-button"]').click();
+    cy.register('Thiago André', 'thiagoandre@email.com', 'Password123');
   });
 });
