3c508e892f910cf02f9b18b662f79f3aef86def0
Thiago Nunes <devthiart@gmail.com>
2025-11-19 14:52:46 -0300

    Centralizating documentation on Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "4jv5ta",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
